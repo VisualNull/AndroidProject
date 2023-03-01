c697d632597eeae5e790db67b8d5b53b63a72664
Visua1NULL <visualnull@163.com>
2023-03-01 17:11:06 +0800

    2023/3/01/17/10

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -3,8 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,8 +33,19 @@ public class MainActivity3 extends AppCompatActivity {
     private Runnable mSetText=new Runnable() {
         @Override
         public void run() {
-            TextView textView=findViewById(R.id.textView3);
-            textView.setText(R.string.Hello);
+            TextView textView3=findViewById(R.id.textView3);
+            TextView textView4=findViewById(R.id.textView4);
+            TextView textView5=findViewById(R.id.textView5);
+            textView3.setText(R.string.Hello);
+            textView3.setTextColor(Color.RED);
+            textView3.setBackgroundColor(Color.BLUE);
+            textView4.setText(R.string.Hello);
+            textView4.setTextColor(Color.BLUE);
+            textView4.setBackgroundColor(Color.GREEN);
+            textView5.setText(R.string.Hello);
+            textView5.setTextColor(Color.GREEN);
+            textView5.setBackgroundResource(R.color.red);
+
         }
     };
 }
